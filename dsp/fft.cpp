f9cad2385c59c88c20d6f98f46f3307de8784d2f
Hal@shurabaP <harunyann@hotmail.com>
2020-04-30 02:17:05 +0900

    aggregate FFT functions into fft.hpp & fft.cpp

--- dsp/fft.cpp
+++ dsp/fft.cpp
@@ -22,6 +22,106 @@
 #include <cmath>
 #include <cstdlib>
 
+//-----------------------------------------------------------------------------
+// FFT, IFFT and minimum phase analysis
+void InitializeForwardRealFFT(int fft_size, ForwardRealFFT *forward_real_fft) {
+    forward_real_fft->fft_size = fft_size;
+    forward_real_fft->waveform = new double[fft_size];
+    forward_real_fft->spectrum = new fft_complex[fft_size];
+    forward_real_fft->forward_fft = fft_plan_dft_r2c_1d(fft_size, forward_real_fft->waveform,
+                                                        forward_real_fft->spectrum, FFT_ESTIMATE);
+}
+
+void DestroyForwardRealFFT(ForwardRealFFT *forward_real_fft) {
+    fft_destroy_plan(forward_real_fft->forward_fft);
+    delete[] forward_real_fft->spectrum;
+    delete[] forward_real_fft->waveform;
+}
+
+void InitializeInverseRealFFT(int fft_size, InverseRealFFT *inverse_real_fft) {
+    inverse_real_fft->fft_size = fft_size;
+    inverse_real_fft->waveform = new double[fft_size];
+    inverse_real_fft->spectrum = new fft_complex[fft_size];
+    inverse_real_fft->inverse_fft = fft_plan_dft_c2r_1d(fft_size, inverse_real_fft->spectrum,
+                                                        inverse_real_fft->waveform, FFT_ESTIMATE);
+}
+
+void DestroyInverseRealFFT(InverseRealFFT *inverse_real_fft) {
+    fft_destroy_plan(inverse_real_fft->inverse_fft);
+    delete[] inverse_real_fft->spectrum;
+    delete[] inverse_real_fft->waveform;
+}
+
+void InitializeInverseComplexFFT(int fft_size,
+                                 InverseComplexFFT *inverse_complex_fft) {
+    inverse_complex_fft->fft_size = fft_size;
+    inverse_complex_fft->input = new fft_complex[fft_size];
+    inverse_complex_fft->output = new fft_complex[fft_size];
+    inverse_complex_fft->inverse_fft = fft_plan_dft_1d(
+            fft_size, inverse_complex_fft->input, inverse_complex_fft->output, FFT_BACKWARD, FFT_ESTIMATE);
+}
+
+void DestroyInverseComplexFFT(InverseComplexFFT *inverse_complex_fft) {
+    fft_destroy_plan(inverse_complex_fft->inverse_fft);
+    delete[] inverse_complex_fft->input;
+    delete[] inverse_complex_fft->output;
+}
+
+void InitializeMinimumPhaseAnalysis(int fft_size, MinimumPhaseAnalysis *minimum_phase) {
+    minimum_phase->fft_size = fft_size;
+    minimum_phase->log_spectrum = new double[fft_size];
+    minimum_phase->minimum_phase_spectrum = new fft_complex[fft_size];
+    minimum_phase->cepstrum = new fft_complex[fft_size];
+    minimum_phase->inverse_fft =
+            fft_plan_dft_r2c_1d(fft_size, minimum_phase->log_spectrum, minimum_phase->cepstrum, FFT_ESTIMATE);
+    minimum_phase->forward_fft =
+            fft_plan_dft_1d(fft_size, minimum_phase->cepstrum, minimum_phase->minimum_phase_spectrum, FFT_FORWARD,
+                            FFT_ESTIMATE);
+}
+
+void GetMinimumPhaseSpectrum(const MinimumPhaseAnalysis *minimum_phase) {
+    // Mirroring
+    for (int i = minimum_phase->fft_size / 2 + 1;
+         i < minimum_phase->fft_size; ++i)
+        minimum_phase->log_spectrum[i] = minimum_phase->log_spectrum[minimum_phase->fft_size - i];
+
+    // This fft_plan carries out "forward" FFT.
+    // To carriy out the Inverse FFT, the sign of imaginary part
+    // is inverted after FFT.
+    fft_execute(minimum_phase->inverse_fft);
+    minimum_phase->cepstrum[0][1] *= -1.0;
+    for (int i = 1; i < minimum_phase->fft_size / 2; ++i) {
+        minimum_phase->cepstrum[i][0] *= 2.0;
+        minimum_phase->cepstrum[i][1] *= -2.0;
+    }
+    minimum_phase->cepstrum[minimum_phase->fft_size / 2][1] *= -1.0;
+    for (int i = minimum_phase->fft_size / 2 + 1; i < minimum_phase->fft_size; ++i) {
+        minimum_phase->cepstrum[i][0] = 0.0;
+        minimum_phase->cepstrum[i][1] = 0.0;
+    }
+
+    fft_execute(minimum_phase->forward_fft);
+
+    // Since x is complex number, calculation of exp(x) is as following.
+    // Note: This FFT library does not keep the aliasing.
+    double tmp;
+    for (int i = 0; i <= minimum_phase->fft_size / 2; ++i) {
+        tmp = exp(minimum_phase->minimum_phase_spectrum[i][0] / minimum_phase->fft_size);
+        minimum_phase->minimum_phase_spectrum[i][0] =
+                tmp * cos(minimum_phase->minimum_phase_spectrum[i][1] / minimum_phase->fft_size);
+        minimum_phase->minimum_phase_spectrum[i][1] =
+                tmp * sin(minimum_phase->minimum_phase_spectrum[i][1] / minimum_phase->fft_size);
+    }
+}
+
+void DestroyMinimumPhaseAnalysis(MinimumPhaseAnalysis *minimum_phase) {
+    fft_destroy_plan(minimum_phase->forward_fft);
+    fft_destroy_plan(minimum_phase->inverse_fft);
+    delete[] minimum_phase->cepstrum;
+    delete[] minimum_phase->log_spectrum;
+    delete[] minimum_phase->minimum_phase_spectrum;
+}
+
 void cdft(int n, int isgn, double *a, int *ip, double *w);
 
 void rdft(int n, int isgn, double *a, int *ip, double *w);
--- dsp/fft.hpp
+++ dsp/fft.hpp
@@ -3,7 +3,86 @@
 #ifndef __DSP_FFT_HPP__
 #define __DSP_FFT_HPP__
 
-#include "fft_util.hpp"
+// Complex number for FFT
+typedef double fft_complex[2];
+// Struct used for FFT
+typedef struct {
+    int n;
+    int sign;
+    unsigned int flags;
+    fft_complex *c_in;
+    double *in;
+    fft_complex *c_out;
+    double *out;
+    double *input;
+    int *ip;
+    double *w;
+} fft_plan;
+
+// Commands for FFT (This is the same as FFTW)
+#define FFT_FORWARD 1
+#define FFT_BACKWARD 2
+#define FFT_ESTIMATE 3
+
+// Forward FFT in the real sequence
+typedef struct {
+    int fft_size;
+    double *waveform;
+    fft_complex *spectrum;
+    fft_plan forward_fft;
+} ForwardRealFFT;
+
+// Inverse FFT in the real sequence
+typedef struct {
+    int fft_size;
+    double *waveform;
+    fft_complex *spectrum;
+    fft_plan inverse_fft;
+} InverseRealFFT;
+
+// Inverse FFT in the complex sequence
+typedef struct {
+    int fft_size;
+    fft_complex *input;
+    fft_complex *output;
+    fft_plan inverse_fft;
+} InverseComplexFFT;
+
+// Minimum phase analysis from logarithmic power spectrum
+typedef struct {
+    int fft_size;
+    double *log_spectrum;
+    fft_complex *minimum_phase_spectrum;
+    fft_complex *cepstrum;
+    fft_plan inverse_fft;
+    fft_plan forward_fft;
+} MinimumPhaseAnalysis;
+
+//-----------------------------------------------------------------------------
+// These functions are used to speed up the processing.
+// Forward FFT
+void InitializeForwardRealFFT(int fft_size, ForwardRealFFT *forward_real_fft);
+
+void DestroyForwardRealFFT(ForwardRealFFT *forward_real_fft);
+
+// Inverse FFT
+void InitializeInverseRealFFT(int fft_size, InverseRealFFT *inverse_real_fft);
+
+void DestroyInverseRealFFT(InverseRealFFT *inverse_real_fft);
+
+// Inverse FFT (Complex)
+void InitializeInverseComplexFFT(int fft_size,
+                                 InverseComplexFFT *inverse_complex_fft);
+
+void DestroyInverseComplexFFT(InverseComplexFFT *inverse_complex_fft);
+
+// Minimum phase analysis (This analysis uses FFT)
+void InitializeMinimumPhaseAnalysis(int fft_size,
+                                    MinimumPhaseAnalysis *minimum_phase);
+
+void GetMinimumPhaseSpectrum(const MinimumPhaseAnalysis *minimum_phase);
+
+void DestroyMinimumPhaseAnalysis(MinimumPhaseAnalysis *minimum_phase);
 
 fft_plan fft_plan_dft_1d(int n, fft_complex *in, fft_complex *out, int sign, unsigned int flags);
 
