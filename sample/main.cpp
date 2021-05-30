26b64298e22496c9c1c1386f794eca0a3064ffa2
Hal@shurabaP <harunyann@hotmail.com>
2021-05-30 23:05:09 +0900

    fix install command

--- sample/main.cpp
+++ sample/main.cpp
@@ -2,8 +2,26 @@
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
 
-#include "AnalyzeAperiodicity.hpp"
+#include "data/Waveform.hpp"
+#include "spectrogram//StretchedPartialSpectrogram.hpp"
+#include "spectrogram/WaveformSpectrogram.hpp"
+
+using namespace uzume::vocoder;
 
 int main(void) {
+    auto *in = Waveform::read("hogeeee");
+    auto *inSpec = new WaveformSpectrogram(in);
+
+    class Tam : public TimeAxisMap {
+        double at(double ms) const override final {
+            return ms * 2.0;
+        };
+        double msLength() const override final {
+            return 1000.0;
+        }
+    };
+    auto *tam = new Tam();
+    auto *outSpec = new StretchedPartialSpectrogram(inSpec, tam);
+    auto *out = new Waveform(in->length * 2, in->samplingFrequency);
     return 0;
 }
