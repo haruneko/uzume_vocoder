4a314e58e4771ca67c809f48bf7100da27990b32
Hal@shurabaP <harunyann@hotmail.com>
2021-05-30 22:48:28 +0900

    Change to enable directory hierarchy

--- sample/main.cpp
+++ sample/main.cpp
@@ -2,8 +2,7 @@
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
 
-#include "vocoder/WaveformSpectrogram.hpp"
-#include "vocoder/StretchedPartialSpectrogram.hpp"
+#include "AnalyzeAperiodicity.hpp"
 
 int main(void) {
     return 0;
