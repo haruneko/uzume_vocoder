7e42a5902c2d30fd421615efb8fc1272e982ea3d
Hal@shurabaP <harunyann@hotmail.com>
2021-05-30 21:19:45 +0900

    Fixing to add sample

---
+++ sample/main.cpp
@@ -0,0 +1,10 @@
+// Copyright 2021 Hal@shurabaP.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+#include "vocoder/WaveformSpectrogram.hpp"
+#include "vocoder/StretchedPartialSpectrogram.hpp"
+
+int main(void) {
+    return 0;
+}
